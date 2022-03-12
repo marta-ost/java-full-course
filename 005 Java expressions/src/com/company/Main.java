f86d20a061501aa55c75e6ab21e2c83fe4110796
Marta <marta.ostrycharz5@gmail.com>
2022-03-12 21:16:19 +0100

    added 005-006

---
+++ 005 Java expressions/src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // expression = operands & operators
+        // operands =   values, variables, numbers, quantity
+        // operators =  + - * / %
+
+        int friends = 10;
+
+        //friends = friends + 1;
+        //friends = friends - 1;
+        //friends = friends * 2;
+        //friends = friends / 2;
+
+        //friends = friends % 3;
+        //friends = friends % 2;
+
+        //friends++;
+        //friends--;
+
+        double doubleFriends = (double) friends / 3;
+
+        System.out.println(doubleFriends);
+    }
+}

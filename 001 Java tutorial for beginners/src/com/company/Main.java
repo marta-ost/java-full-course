fccb8acb1ec54bded2ad3aae1a9d250a006948d8
Marta <marta.ostrycharz5@gmail.com>
2022-03-12 21:08:49 +0100

    added 001-002

---
+++ 001 Java tutorial for beginners/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //This is a comment
+        /* This
+        is a
+        comment
+         */
+
+        //System.out.print("I love pizza\n");
+        //System.out.println("\tI love pizza");
+        //System.out.println("\"I love pizza\"");
+        //System.out.println("\\I love pizza");
+
+        System.out.println("I love pizza");
+        System.out.println("It's really good!");
+    }
+}
---
+++ 002 Java variables/src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //int x; declaration
+        //x = 123; assignment
+        int x = 123; //initialization
+
+        //long x = 3233213367313223321L;
+        //byte x = 100;
+
+        //float y = 3.14f;
+        double y = 3.14;
+
+        //boolean z = false;
+        boolean z = true;
+
+        char symbol = '@';
+
+        String name = "Bro";
+
+        System.out.println("My number is: " + y);
+        System.out.println(z);
+        System.out.println(symbol);
+        System.out.println("Hello " + name);
+    }
+}

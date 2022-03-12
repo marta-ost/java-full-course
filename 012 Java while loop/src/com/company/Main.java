799f697517c23ed4aabd95fac4592c07e355e84f
Marta <marta.ostrycharz5@gmail.com>
2022-03-12 22:29:59 +0100

    added 011-012

---
+++ 012 Java while loop/src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // while loop = executes a block of code as long as it's condition remains true
+
+        Scanner scanner = new Scanner(System.in);
+        String name = "";
+
+        while (name.isBlank()) {
+            System.out.print("Enter your name: ");
+            name = scanner.nextLine();
+        }
+
+        //do {
+        //    System.out.print("Enter your name: ");
+        //    name = scanner.nextLine();
+        //} while (name.isBlank());
+
+        System.out.println("Hello " + name);
+    }
+}

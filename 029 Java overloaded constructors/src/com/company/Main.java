2cf12e1d4515c41aad909c845827c6e93ab80b31
Marta <marta.ostrycharz5@gmail.com>
2022-03-15 14:32:29 +0100

    added 029

---
+++ 029 Java overloaded constructors/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // overloaded constructors = multiple constructors within a class with the same name,
+        //                           but have different parameters
+        //                           name + parameters = signature
+
+        Pizza pizza = new Pizza("thick crust", "tomato", "mozzarella", "pepperoni");
+        System.out.println("Here are the ingredients of your pizza: ");
+        System.out.println(pizza.bread);
+        System.out.println(pizza.sauce);
+        System.out.println(pizza.cheese);
+        System.out.println(pizza.topping);
+    }
+}

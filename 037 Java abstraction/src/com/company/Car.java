35aa728df598b8de226b82ea2d7315bbee0eb488
Marta <marta.ostrycharz5@gmail.com>
2022-03-15 18:10:20 +0100

    added 037

---
+++ 037 Java abstraction/src/com/company/Car.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Car extends Vehicle {
+
+    @Override
+    void go() {
+        System.out.println("The driver is driving the car");
+    }
+}
---
+++ 037 Java abstraction/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // abstract = abstract classes cannot be instantiated, but they can have a subclass
+        //            abstract methods are declared without an implementation
+
+        //Vehicle vehicle = new Vehicle();
+        Car car = new Car();
+
+        car.go();
+    }
+}

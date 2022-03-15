c510c8d4570ff7faa7a02825356be72943743e2e
Marta <marta.ostrycharz5@gmail.com>
2022-03-15 14:52:21 +0100

    added 030

---
+++ 030 Java toString method/src/com/company/Car.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Car {
+
+    String make = "Ford";
+    String model = "Mustang";
+    String color = "red";
+    int year = 2021;
+
+    public String toString() {
+        return make + "\n" + model + "\n" + color + "\n" + year;
+    }
+}
---
+++ 030 Java toString method/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // toString = special method that all objects inherit,
+        //            that returns a string that "textually represents" an object.
+        //            can be used both implicitly and explicitly
+
+        Car car = new Car();
+
+        //System.out.println(car.make);
+        //System.out.println(car.model);
+        //System.out.println(car.color);
+        //System.out.println(car.year);
+
+        //System.out.println(car.toString());
+        System.out.println(car);
+    }
+}

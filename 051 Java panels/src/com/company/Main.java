e3f5a4baa62de0099a13839d2ff28d9ab13164e4
Marta <marta.ostrycharz5@gmail.com>
2022-03-17 12:41:49 +0100

    added 051

---
+++ 051 Java panels/src/com/company/Main.java
@@ -0,0 +1,52 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //JPanel = a GUI component that functions as a container to hold other components
+
+        ImageIcon icon = new ImageIcon("icon.png");
+
+        JLabel label = new JLabel();
+        label.setText("Hello");
+        label.setIcon(icon);
+        //label.setVerticalAlignment(JLabel.BOTTOM);
+        //label.setHorizontalAlignment(JLabel.LEFT);
+        label.setBounds(80, 80, 200, 120); //relative to the component that it's within - label/frame
+
+        JPanel redPanel = new JPanel();
+        redPanel.setBackground(Color.red);
+        redPanel.setBounds(0, 0, 250, 250);
+        //redPanel.setLayout(new BorderLayout());
+        redPanel.setLayout(null);
+
+        JPanel bluePanel = new JPanel();
+        bluePanel.setBackground(Color.blue);
+        bluePanel.setBounds(250, 0, 250, 250);
+        //bluePanel.setLayout(new BorderLayout());
+        bluePanel.setLayout(null);
+
+        JPanel greenPanel = new JPanel();
+        greenPanel.setBackground(Color.green);
+        greenPanel.setBounds(0, 250, 500, 250);
+        //greenPanel.setLayout(new BorderLayout());
+        greenPanel.setLayout(null);
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        frame.setSize(750, 750);
+        frame.setVisible(true);
+
+        //bluePanel.add(label);
+        greenPanel.add(label);
+
+        frame.add(redPanel);
+        frame.add(bluePanel);
+        frame.add(greenPanel);
+    }
+}

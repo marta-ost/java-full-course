31575d6c94632238fefff1a4de08a65516edcc50
Marta <marta.ostrycharz5@gmail.com>
2022-03-18 16:20:02 +0100

    added 059

---
+++ 059 Java textfield/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // JTextfield = a GUI textbox component that can be used to add, set or get text
+
+        new MyFrame();
+    }
+}
---
+++ 059 Java textfield/src/com/company/MyFrame.java
@@ -0,0 +1,42 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyFrame extends JFrame implements ActionListener {
+
+    JButton button;
+    JTextField textField;
+
+    MyFrame() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new FlowLayout());
+
+        button = new JButton("Submit");
+        button.addActionListener(this);
+
+        textField = new JTextField();
+        textField.setPreferredSize(new Dimension(250, 40));
+        textField.setFont(new Font("Consolas", Font.PLAIN, 35));
+        textField.setForeground(new Color(0x00FF00));
+        textField.setBackground(Color.BLACK);
+        textField.setCaretColor(Color.WHITE);
+        textField.setText("username");
+
+        this.add(button);
+        this.add(textField); //make sure to add before pack
+        this.pack();
+        this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == button) {
+            System.out.println("Welcome " + textField.getText());
+            textField.setEditable(false);
+            button.setEnabled(false); //after we submit the username, we can no longer submit another
+        }
+    }
+}

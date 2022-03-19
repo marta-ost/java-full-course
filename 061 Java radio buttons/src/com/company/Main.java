0f3f009e3e4b5c9a259aed48d77b1e34d88d374c
Marta <marta.ostrycharz5@gmail.com>
2022-03-19 19:52:23 +0100

    added 061

---
+++ 061 Java radio buttons/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // JRadioButton = one or more buttons in a grouping in which only 1 may be selected per group
+
+        new MyFrame();
+    }
+}

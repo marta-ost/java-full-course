46831e0729acbe3743afc8ff028210880878760a
Marta <marta.ostrycharz5@gmail.com>
2022-03-20 17:04:45 +0100

    added 065

---
+++ 065 Java menubar/src/com/company/MyFrame.java
@@ -0,0 +1,83 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
+public class MyFrame extends JFrame implements ActionListener {
+
+    JMenuBar menuBar;
+
+    JMenu fileMenu;
+    JMenu editMenu;
+    JMenu helpMenu;
+
+    JMenuItem loadItem;
+    JMenuItem saveItem;
+    JMenuItem exitItem;
+
+    ImageIcon loadIcon;
+    ImageIcon saveIcon;
+    ImageIcon exitIcon;
+
+    MyFrame() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500, 500);
+        this.setLayout(new FlowLayout());
+
+        loadIcon = new ImageIcon("load.png");
+        saveIcon = new ImageIcon("save.png");
+        exitIcon = new ImageIcon("exit.png");
+
+        menuBar = new JMenuBar();
+
+        fileMenu = new JMenu("File");
+        editMenu = new JMenu("Edit");
+        helpMenu = new JMenu("Help");
+
+        loadItem = new JMenuItem("Load");
+        saveItem = new JMenuItem("Save");
+        exitItem = new JMenuItem("Exit");
+
+        loadItem.addActionListener(this);
+        saveItem.addActionListener(this);
+        exitItem.addActionListener(this);
+
+        loadItem.setIcon(loadIcon);
+        saveItem.setIcon(saveIcon);
+        exitItem.setIcon(exitIcon);
+
+        fileMenu.setMnemonic(KeyEvent.VK_F); // alt + f for file //keyboard shortcuts
+        editMenu.setMnemonic(KeyEvent.VK_E); // alt + e for edit
+        helpMenu.setMnemonic(KeyEvent.VK_H); // alt + h for help
+
+        loadItem.setMnemonic(KeyEvent.VK_L); // l for load
+        saveItem.setMnemonic(KeyEvent.VK_S); // s for save
+        exitItem.setMnemonic(KeyEvent.VK_E); // e for exit
+
+        fileMenu.add(loadItem);
+        fileMenu.add(saveItem);
+        fileMenu.add(exitItem);
+
+        menuBar.add(fileMenu);
+        menuBar.add(editMenu);
+        menuBar.add(helpMenu);
+
+        this.setJMenuBar(menuBar);
+
+        this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == loadItem) {
+            System.out.println("You loaded a file");
+        } else if (e.getSource() == saveItem) {
+            System.out.println("You saved a file");
+        } else if (e.getSource() == exitItem) {
+            System.exit(0);
+        }
+    }
+}

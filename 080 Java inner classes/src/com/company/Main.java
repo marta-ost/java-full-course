1288fc6e7b8f0cd31c6b456a80186412dfdf578a
Marta <marta.ostrycharz5@gmail.com>
2022-03-25 17:25:36 +0100

    added 080

---
+++ 080 Java inner classes/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // inner class = a class inside of another class.
+        //               useful if a class should be limited in scope.
+        //               usually private, but not necessary
+        //               helps group classes that belong together
+        //               extremely useful for listeners for specific events
+        //               precursor for anonymous inner classes
+
+        Outside out = new Outside();
+        Outside.Inside in = out.new Inside();
+
+        //System.out.println(out.x + in.y);
+        in.Greeting();
+    }
+}

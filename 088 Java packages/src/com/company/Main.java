24a9ad3e9f67fd7ea6829fd6011ccf702e2c4a15
Marta <marta.ostrycharz5@gmail.com>
2022-03-28 21:41:37 +0200

    added 088

---
+++ 088 Java packages/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import javax.swing.Icon; // we can explore packages and open this specific package
+                         // our project folder - external libraries: java.desktop - javax - swing
+
+import Tools.Toolbox;
+//import Tools.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Toolbox toolbox = new Toolbox();
+    }
+}
